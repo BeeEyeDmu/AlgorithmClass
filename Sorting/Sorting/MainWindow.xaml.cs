56334d89b5eca11e729d703e474b74121d982a52
BeeEye Dmu <bikang@konyang.ac.kr>
2019-09-05 11:00:04 +0900

    Sorting Program

--- Sorting/Sorting/MainWindow.xaml.cs
+++ Sorting/Sorting/MainWindow.xaml.cs
@@ -20,9 +20,48 @@ namespace Sorting
   /// </summary>
   public partial class MainWindow : Window
   {
+    const int SIZE = 100000;
+    int[] a = new int[SIZE];
+    int N;
+
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    private void BtnCreate_Click(object sender, RoutedEventArgs e)
+    {
+      N = int.Parse(txtNo.Text);
+      Random r = new Random();
+      for (int i = 0; i < N; i++)
+        a[i] = r.Next();
+      PrintArray(a);
+    }
+
+    // 버블소트
+    private void BtnQuad_Click(object sender, RoutedEventArgs e)
+    {
+      for (int i = N - 1; i > 0; i--)
+        for (int j = 0; j < i; j++)
+        {
+          if (a[j] > a[j + 1])
+          {
+            int t = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = t;
+          }
+        }
+      PrintArray(a);
+    }
+
+    private void PrintArray(int[] a)
+    {
+      Console.WriteLine("Print a[]");
+      for(int i=0; i<10; i++)
+        Console.WriteLine(a[i]);
+      Console.WriteLine("...");
+      for(int i =N-10; i<N; i++)
+        Console.WriteLine(a[i]);
     }
   }
 }
