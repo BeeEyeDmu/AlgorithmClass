aea23245dd9a9ea659c0c3f9098a97017c15553e
BeeEye Dmu <bikang@konyang.ac.kr>
2019-09-26 10:59:05 +0900

    수업중 코딩(Closest Pair)

--- ClosestPair/ClosestPair/MainWindow.xaml.cs
+++ ClosestPair/ClosestPair/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace ClosestPair
   /// </summary>
   public partial class MainWindow : Window
   {
+    const int P = 100;
+    Point[] points = new Point[P];
+
     public MainWindow()
     {
       InitializeComponent();
+    }
+
+    // 생성
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+      canvas1.Children.Clear();
+      MakePointArray();
+    }
+
+    private void MakePointArray()
+    {
+      Random r = new Random();
+
+      for(int i=0; i<P; i++)
+      {
+        points[i].X = r.Next(500);
+        points[i].Y = r.Next(500);
+      }
+
+      foreach(var p in points)
+      {
+        Rectangle rect = new Rectangle();
+        rect.Width = 3;
+        rect.Height = 3;
+        rect.Stroke = Brushes.Black;
+        Canvas.SetLeft(rect, p.X-1);
+        Canvas.SetTop(rect, p.Y-1);
+
+        canvas1.Children.Add(rect);
+      }
+    }
+
+    // 찾기버튼
+    private void Button_Click_1(object sender, RoutedEventArgs e)
+    {
+      double min = double.MaxValue;
+
+      for(int i=0; i<P; i++)
+        for(int j=i+1; j<P; j++)
+        {
+          double d = Dist(i, j);
+          // d가 min 보다 작으면
+          // d, i, j를 저장
+          
+        }
+    }
+
+    // points[i]와 points[j]의 거리
+    private double Dist(int i, int j)
+    {
+      // Math.Pow(x, 2) 사용, 또는 x*x
+      // Math.Sqrt() 사용
     }
   }
 }
