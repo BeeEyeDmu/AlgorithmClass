728505c173024eda472043bdb18c01e9a6141dbe
byungik kang <bikang@hotmail.co.kr>
2019-10-15 11:09:25 +0900

    HANOI

---
+++ Hanoi/Hanoi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hanoi(4, 'A', 'C', 'B'); // A에서 B를 이용해 C로 이동
+
+        }
+
+        private static void Hanoi(int n, char from, char to, char by)
+        {
+            if (n == 1)
+                Console.WriteLine("Move : {0} -> {1}", from, to);
+            else
+            {
+                Hanoi(n - 1, from, by, to);
+                Console.WriteLine("Move : {0} -> {1}", from, to);
+                Hanoi(n - 1, by, to, from);
+            }
+        }
+    }
+}
