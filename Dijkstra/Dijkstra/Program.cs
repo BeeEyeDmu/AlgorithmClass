eeca1814128d5e5d9a77379b39ce830aef33bf75
byungik kang <bikang@hotmail.co.kr>
2019-10-31 09:58:57 +0900

    Update Program.cs

--- Dijkstra/Dijkstra/Program.cs
+++ Dijkstra/Dijkstra/Program.cs
@@ -32,7 +32,7 @@ namespace Dijkstra
         { 0, 0, 0, 0, 0, 0, 9, 5, 15, 0}
       };
 
-      ShortestPath(graph, 1); // 대전출발
+      ShortestPath(graph, 5); // 서울출발
     }
 
     // src는 출발점
@@ -47,7 +47,7 @@ namespace Dijkstra
 
       D[src] = 0;
 
-      for(int i=0; i<V; i++) // V ? V-1
+      for(int i=0; i<V-1; i++) // 반복횟수: 처음 시작 도시 빼고 나머지 도시수 만큼 반복
       {
         int minIndex = MinDistance();  // 최단경로가 계산된 도시의 인덱스
         spt[minIndex] = true;
@@ -55,12 +55,14 @@ namespace Dijkstra
 
         // update D[] of 인접 버텍스
         // 4번 라인
+        // 모든 버텍스 v에 대해서
         for (int v = 0; v < V; v++)
-          if (!spt[v] && graph[i, v] != 0
-            && D[i] != int.MaxValue
-            && D[i] + graph[i, v] < D[v])
+          if (!spt[v]   // 최단경로가 결정되지 않았고
+              && graph[minIndex, v] != 0   // 자기 자신이나 연결되지 않은 도시는 뺴고(연결된 도시 중에서)
+              //&& D[minIndex] != int.MaxValue   // D[] 값이 계산되어 있고, 없어도 될듯...
+              && D[minIndex] + graph[minIndex, v] < D[v]) // i와 인접한 v가 결정된 D[i]+에지 가중치를 더한 것이 D[v]보다 작으면
           {
-            D[v] = D[i] + graph[i, v];
+            D[v] = D[minIndex] + graph[minIndex, v];  // 새로운 값으로 D[v]를 업데이트한다
           }
 
         Console.WriteLine("iteration: {0}", i);
